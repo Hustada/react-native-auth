69381285e8976d2066faa6b511918358ee811b16
Mark Hustad <hustada80@gmail.com>
2017-09-02 17:33:38 -0500

    add styles

--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -3,16 +3,43 @@ import { TextInput, View, Text } from 'react-native';
 	
 	//pass in label text as prop
 	const Input = ({ label, value, onChangeText }) => {
+		const { inputStyle, lableStyle, containerStyle } = styles;
+
 		return(
-			<View>
-				<Text>{label}</Text>
+			<View style={containerStyle}>
+				<Text style={lableStyle}>{label}</Text>
 				<TextInput
+					style={inputStyle}
 					value={value}
 					onChangeText={onChangeText}
-					style={{ height: 20, width: 20 }}
+					style={{ height: 20, width: 100 }}
 				/>
 			</View>
 		);
+	};
+//both input and text are children of the view tag
+//whenver we have siblings with a flex property. Thats how we allocate a proportion of available space to each one. add the values of flex 2 + 1 equals 3. So 1/3 and 2/3 for each respectively.
+	const styles = {
+		inputStyle: {
+			color: '#000',
+			paddingRight: 5,
+			paddingLeft: 5,
+			fontSize: 18,
+			lineHeight: 23,
+			flex: 2
+		},
+		lableStyle: {
+			fontSize: 18,
+			paddingLeft: 20,
+			flex: 1
+		},
+		containerStyle: {
+			height: 40,
+			flex: 1,
+			flexDirection: 'row',
+			alignItems: 'center'
+		}
+
 	};
 
 
