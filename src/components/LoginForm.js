f5667f6d9278609c53e70a0e820fc3a65be92334
Mark Hustad <hustada80@gmail.com>
2017-09-03 00:41:22 -0500

    create activityindicator spinner

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
+import firebase from 'firebase';
 import { Button, Card, CardSection, Input } from './common';
 
 //Text inputs have zero styling by default
 // react holds the text 'state' outside of user input
+//define onButtonPress method
+// always rerender components by using component level state
+// if user both fails to sign in and create a new user then show error
+
 class LoginForm extends Component {
 	//initialize state object
-	state = { email: '', password: '' };
+	state = { email: '', password: '', error: '' };
+	
+	onButtonPress() {
+		const { email, password } = this.state; // pass in email and password
+
+		this.setState({ error: '' }); //set this to empty object
+
+		firebase.auth().signInWithEmailAndPassword(email, password).catch(() => {
+			firebase.auth().createUserWithEmailAndPassword(email, password).catch(() => {
+				this.setState({ error: 'Authentication Failed.' });
+			});
+		}); //catch errors
+	}
+
 	render() {
 		return (
 			<Card>
@@ -28,13 +47,26 @@ class LoginForm extends Component {
 				/>
 				</CardSection>
 
+				
+				<Text style={styles.errorTextStyle}>
+					{this.state.error}
+				</Text>
+
 				<CardSection>
-					<Button>
+					<Button onPress={this.onButtonPress.bind(this)}>
 						Login
 					</Button>
 				</CardSection>
 			</Card>
 		);
+	}
+}
+
+const styles = {
+	errorTextStyle: {
+		fontSize: 20,
+		alignSelf: 'center',
+		color: 'red'
 	}
 }
 
